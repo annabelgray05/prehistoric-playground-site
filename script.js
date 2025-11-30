0c6b5bb6b6cfbde3633351ec1d925eb951b99f5b
annabelgray05 <annabelgray05@gmail.com>
2025-11-30 16:26:54 +0000

    Add script to set footer year automatically

---
+++ script.js
@@ -0,0 +1,7 @@
+// Set footer year automatically
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
